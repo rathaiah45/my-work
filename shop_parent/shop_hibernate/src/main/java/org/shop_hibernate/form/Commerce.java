5b43aaeae3fe380b528c50060a4edbe412eb34ab
rathaiah45 <yasamrathaiah@gmail.com>
2017-05-30 23:05:57 +0530

    hibernate changes

---
+++ shop_parent/shop_hibernate/src/main/java/org/shop_hibernate/form/Commerce.java
@@ -0,0 +1,41 @@
+package org.shop_hibernate.form;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+@Entity
+@Table(name = "COMMERCE")
+@Cache(usage = CacheConcurrencyStrategy.READ_ONLY, region = "commerce")
+public class Commerce implements Serializable {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	@Column(name = "DEP_NAME")
+	private String name;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
---
+++ shop_parent/shop_hibernate/src/main/java/org/shop_hibernate/test/TestHibernateCache.java
@@ -0,0 +1,53 @@
+package org.shop_hibernate.test;
+
+import org.hibernate.Session;
+import org.hibernate.cfg.AnnotationConfiguration;
+import org.shop_hibernate.form.Commerce;
+
+public class TestHibernateCache {
+
+	public static void main(String[] args) {
+		store();
+		try {
+			Session session = new AnnotationConfiguration()
+					.configure("hibernate.cfg.xml").buildSessionFactory()
+					.openSession();/*
+									 * HibernateUtil.getSessionFactory().openSession
+									 * ();
+									 */
+			session.beginTransaction();
+
+			Commerce cmx = (Commerce) session.load(Commerce.class, new Integer(
+					1));
+			System.out.println(cmx.getName());
+			cmx = (Commerce) session.load(Commerce.class, new Integer(1));
+			System.out.println(cmx.getName());
+			session.getTransaction().commit();
+			session.close();
+
+			Session another = new AnnotationConfiguration()
+					.configure("hibernate.cfg.xml").buildSessionFactory()
+					.openSession();
+			another.beginTransaction();
+
+			Commerce cm = (Commerce) another.load(Commerce.class,
+					new Integer(1));
+			System.out.println(cm.getName());
+		} catch (Exception e) {
+
+		} finally {
+			HibernateUtil.shutdown();
+		}
+	}
+
+	public static void store() {
+		Session session = new AnnotationConfiguration()
+				.configure("hibernate.cfg.xml").buildSessionFactory()
+				.openSession();
+		session.beginTransaction();
+		Commerce cmd = new Commerce();
+		cmd.setName("Agilent");
+		session.save(cmd);
+		session.getTransaction().commit();
+	}
+}
